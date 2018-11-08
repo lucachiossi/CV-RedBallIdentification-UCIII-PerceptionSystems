792215cfa820ac340700b85afda1b2c17c28ddaa
Unknown <chiossi.lu@gmail.com>
2018-11-08 15:42:54 +0100

    english description

--- _LucaExperiments/LucaTest.cpp
+++ _LucaExperiments/LucaTest.cpp
@@ -7,12 +7,15 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	cout << "inizio programma" << endl;
 
+	// declaration matrix
 	// dichiarazione matrice
 	Mat img;
 
+	// read image
 	// lettura immagine
 	img = imread("C:/Users/lucac_000/Desktop/Luca/UNIVERSITA/__MAGISTRALE__/_II_anno/M1_Sistemas_de_Percepcion/_Laboratory/Lab1/mandril.jpg");
 
+	// check reading result
 	// controllo esito lettura
 	if (!img.data) {
 		cout << "errore lettura immagine mandril.jpg" << endl;
@@ -20,9 +23,11 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
+	// create a window to show the image
 	// creazione finestra per mostrare la immagine
 	namedWindow("original", CV_WINDOW_AUTOSIZE);
 
+	// show image in the window
 	// mostrare immagine nella finestra creata
 	imshow("original", img);
 	waitKey(0);
