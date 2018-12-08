c3830a44259a1bdcd3a35bc909b1fa89296209ea
Unknown <chiossi.lu@gmail.com>
2018-12-08 18:58:21 +0100

    added circle detection comparisons

---
+++ _LucaExperiments/PearsonCorrelation.h
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+#ifndef ADD_H
+#define ADD_H
+
+double pearsoncoeff(vector<double> X, vector<double> Y);
+
+#endif
